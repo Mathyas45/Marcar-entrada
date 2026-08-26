c9bb7724d2a6b681ebcca0a3c3e53499eca7828a
mcoronadoext_utpGH <mcoronadoext@utp.edu.pe>
2026-08-26 18:55:33 -0500

    Instalar Stealth Plugin antibots

--- jibble.js
+++ jibble.js
@@ -1,4 +1,6 @@
-const { chromium } = require('playwright');
+const { chromium } = require('playwright-extra');
+const stealth = require('puppeteer-extra-plugin-stealth')();
+chromium.use(stealth);
 
 const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 const getRandomJitter = (minMinutes, maxMinutes) => {
